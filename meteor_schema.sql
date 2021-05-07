092185f90ee70603daf1e3c4e291215f32a3ca6f
axfrancois123 <74634280+axfrancois123@users.noreply.github.com>
2021-05-06 20:05:59 -0500

    Add files via upload

---
+++ meteor_schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE meteorite_landings(
+
+    name VARCHAR(30),
+    id INT PRIMARY KEY,
+    nametype varchar(30),
+    recclass varchar(30),
+    mass FLOAT,
+    fall VARCHAR(10),
+    year DATE,
+    reclat FLOAT,
+    reclong FLOAT
+);
